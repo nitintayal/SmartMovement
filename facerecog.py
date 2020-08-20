09c0e7d6a9f3d142dbfbd769856c8c8f476bdd94
debangagogoi <30667962+debangagogoi@users.noreply.github.com>
2020-08-20 21:05:09 +0530

    Add files via upload

---
+++ facerecog.py
@@ -0,0 +1,47 @@
+import cv2
+import sys
+
+#cascPath = sys.argv[1]
+eyeCascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    eyes = eyeCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    #for (x, y, w, h) in faces:
+    #    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    #    cv2.imshow('Video', frame)
+for (x1, y1, w1, h1) in eyes:
+    cv2.rectangle(frame, (x1, y1), (x1+w1, y1+h1), (0, 255, 0), 2)
+
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
